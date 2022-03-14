b4e14a5972cbe15331e1afd7e120f802a1475485
Kay Schecker <sayhello@kay-schecker.de>
2022-03-14 20:22:11 +0100

    chore: print version

--- apps/app/src/app/app.component.ts
+++ apps/app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import packageInfo from '../../package.json';
 
 @Component({
   selector: 'bvr-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+
+  public readonly packageInfo = packageInfo;
+
+  constructor() {
+    console.log(this.packageInfo)
+  }
 }
