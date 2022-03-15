b8a21203c65820fa595b3af6475886bc82018b73
Kay Schecker <sayhello@kay-schecker.de>
2022-03-15 03:06:09 +0100

    fix: setup appid

--- apps/app/capacitor.config.ts
+++ apps/app/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'de.blaulicht-app.app',
   appName: 'app',
   webDir: '../../dist/apps/app',
   bundledWebRuntime: false,
